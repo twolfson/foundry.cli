2d6ee6c3cdf5577b99bc9a87d21b08c4515d18f2
Todd Wolfson <todd@twolfson.com>
2015-10-09 05:18:53 -0500

    Fixed up Windows support for PATH

--- test/foundry-cli-test.js
+++ test/foundry-cli-test.js
@@ -22,8 +22,8 @@ describe('foundry.cli running in a directory with `foundry`', function () {
   });
 
   it('added `node_modules/.bin/` to the `PATH`', function () {
+    console.log(this.stdout);
     var actualPaths = JSON.parse(this.stdout).PATH.split(path.delimiter);
-    console.log(actualPaths);
     var expectedPath = path.join(__dirname, 'test-files', 'repo-with-foundry', 'node_modules', '.bin');
     expect(actualPaths).to.contain(expectedPath);
   });
